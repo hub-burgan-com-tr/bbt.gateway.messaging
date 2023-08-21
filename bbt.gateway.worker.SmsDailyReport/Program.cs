7d4060810ed859d44daaf5d488e5d4dab4c94dbd
Sercan Yüksel <srcanyuksel@gmail.com>
2023-08-21 10:35:10 +0300

    Log Added

--- bbt.gateway.worker.SmsDailyReport/Program.cs
+++ bbt.gateway.worker.SmsDailyReport/Program.cs
@@ -5,11 +5,13 @@ using bbt.gateway.worker.SmsDailyReport;
 using Elastic.Apm.NetCoreAll;
 using Microsoft.EntityFrameworkCore;
 
+Console.WriteLine("Starting ...");
 IHost host = Host.CreateDefaultBuilder(args)
     .UseVaultSecrets(typeof(Program))
     .UseSeriLog("entegrasyon")
     .ConfigureServices((context, services) =>
     {
+        Console.WriteLine("DI Starting ...");
         services.AddHostedService<SmsDailyReportWorker>();
         services.AddDbContext<DatabaseContext>(o =>  o.UseSqlServer(context.Configuration.GetConnectionString("DefaultConnection")),ServiceLifetime.Singleton);
         services.AddDbContext<SmsBankingDatabaseContext>(o => o.UseSqlServer(context.Configuration.GetConnectionString("SmsBankingConnection")),ServiceLifetime.Singleton);
